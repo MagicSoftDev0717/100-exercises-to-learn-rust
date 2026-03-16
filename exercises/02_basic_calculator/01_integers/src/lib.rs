aad0facf09e67c6e32d191c881075e3718c7ba03
MagicSoftDev0717 <devsoft0717@gmail.com>
2026-03-17 00:20:01 +0700

    100-02-01_integers

--- exercises/02_basic_calculator/01_integers/src/lib.rs
+++ exercises/02_basic_calculator/01_integers/src/lib.rs
@@ -1,6 +1,8 @@
 fn compute(a: u32, b: u32) -> u32 {
     // TODO: change the line below to fix the compiler error and make the tests pass.
-    let multiplier: u8 = 4;
+    //By added MagicSoftDev0717
+    //let multiplier: u8 = 4;
+    let multiplier: 32 = 4;
     a + b * multiplier
 }
 
